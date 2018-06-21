aa592cb17bab6e7bc5fa39dcbe70f66d533dc2c5
dejanstojanovic <d.stojanovic@hotmail.com>
2018-06-21 09:24:13 +0400

    Update pipeline to use atuhentication

--- Sample.Core.Identity.Api/Startup.cs
+++ Sample.Core.Identity.Api/Startup.cs
@@ -74,12 +74,17 @@ namespace Sample.Core.Identity.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("Cors");
+            app.UseAuthentication();
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseMvc();
+
         }
     }
 }
